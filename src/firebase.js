30edbf1e08ccc44e02db4fb8e5cca5932adf603a
TanishqSinghAnand <anandtanishqs@gmail.com>
2021-04-06 11:07:07 +0530

    Changed Title

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9uI-O54PXQWZMX7DPamo0wx9FHJ3h8S8",
+  authDomain: "my-netflix-e8897.firebaseapp.com",
+  projectId: "my-netflix-e8897",
+  storageBucket: "my-netflix-e8897.appspot.com",
+  messagingSenderId: "344233357880",
+  appId: "1:344233357880:web:7c28f8172b7af7f4832ccd",
+};
